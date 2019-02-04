21f7243196f1b6eccd16686e782a57fd35eed433
福島　Ｋ <kota@fukushimanoMacBook-Pro.local>
2019-02-04 15:19:54 +0900

    一部訂正

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,18 +21,24 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
-
+		bo=true;
+		by=1;
+		s=1;
+		c='a';
+		i=1;
+		l=10000;
+		f=1F;
+		d=1.11;
 
 		// ★ それぞれの変数の中身を出力してください
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 
